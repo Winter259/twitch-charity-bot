3c3726dfd0ef884fb92311756a9c981321f6b8cd
Simon AM <simon@agius-muscat.net>
2015-10-03 23:38:33 +0200

    Moved more code to Twitch class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from pytwitch import *
-from time import *
-from winsound import Beep
 
 """
 Include a file called cfg.py in the same directory as main.py with the following:
@@ -11,28 +9,11 @@ STREAMER_NAME = 'test'              # name of the streamer
 PROMPT_TICK_TIME = 10 * 60  # interval in seconds between when the bot will post prompts
 """
 
-def pause(prompt='', amount=5):
-    ticks = amount
-    print(prompt)
-    while ticks > 0:
-        print('[*] Pause ends in: {}  '.format(ticks), end='\r')
-        sleep(1)
-        ticks -= 1
-    print('[+] Pause ended, continuing now!')
+def main():
+    print('--- Starting Purrbot! ---\n')
+    purrbot = Twitch(NICK, PASS, CHAN)
+    purrbot.run()
 
 
-def beep_speaker(ping_amount=2, delay=1.0):
-    for tick in range(0, ping_amount):
-        Beep(200, 200)
-        sleep(delay)
-
-print('--- Starting Purrbot! ---\n')
-purrbot = Twitch(NICK, PASS, CHAN)
-cycle = 1
-while True:
-    purrbot.check_connection()
-    print('[+] Purrbot is on cycle: {}'.format(cycle))
-    print('[+] Waiting for data input from the channel', end='\r')
-    purrbot.post_in_channel('I am a bot and I am being tested! Cycle: {}'.format(cycle))
-    pause('[+] Waiting for next Cycle', PROMPT_TICK_TIME)
-    cycle += 1
+if __name__ == '__main__':
+    main()
--- pytwitch.py
+++ pytwitch.py
@@ -1,17 +1,41 @@
 import socket
+from winsound import Beep
+from time import sleep
 from cfg import *
+
+def pause(prompt='', amount=5):
+    ticks = amount
+    print(prompt)
+    while ticks > 0:
+        print('[*] Pause ends in: {}  '.format(ticks), end='\r')
+        sleep(1)
+        ticks -= 1
+    print('[+] Pause ended, continuing now!')
 
 HOST = "irc.twitch.tv"  # the Twitch IRC server
 PORT = 6667             # always use port 6667!
-DATA_BUFFER_SIZE = 256
+DATA_BUFFER_SIZE = 1024
+CONNECTION_CHECK_DELAY = 3
 
 class Twitch():
     def __init__(self, name='', token='', channel=''):
         self.name = name
         self.token = token
         self.connection = socket.socket()
+        self.connection_checks = 0
         self.channel = self.connect(channel)
         print('[!] Purrbot initialised and connected!')
+
+    def run(self):
+        cycle = 1
+        while True:
+            print('[+] Purrbot is on cycle: {}'.format(cycle))
+            self.check_connection()
+            self.post_in_channel('I am a bot and I am being tested! Cycle: {}'.format(cycle))
+            pause()
+            print('[+] Holding {} seconds for next prompt'.format(PROMPT_TICK_TIME))
+            sleep(PROMPT_TICK_TIME)
+            cycle += 1
 
     def connect(self, channel=''):
         if len(channel) == 0:
@@ -28,17 +52,26 @@ class Twitch():
                 print('[-] Bot did not manage to connect! Exception occurred: {}'.format(e))
                 exit(0)
 
-    def ping_pong(self, decoded_data):
-        print('[!] Bot responding to the irc ping with a pong!')
+    def receive_data(self):
+        response = self.connection.recv(DATA_BUFFER_SIZE)
+        return response.decode('utf-8')
+
+    def print_response(self):
+        decoded_response = self.receive_data()
+        print('Response: {}'.format(decoded_response))
+
+    def ping_pong(self):
+        print('[!] Purrbot has confirmed its connection to the twitch IRC. Checks so far: {}'.format(self.connection_checks))
         self.connection.send('PONG :tmi.twitch.tv\r\n'.encode('utf-8'))
 
     def check_connection(self):
-        response = self.connection.recv(DATA_BUFFER_SIZE)
-        decoded_response = response.decode('utf-8')
+        print('[?] Purrbot is checking if it is still connected')
+        self.connection_checks += 1
+        decoded_response = self.receive_data()
         if decoded_response == 'PING :tmi.twitch.tv\r\n':
-            self.ping_pong(decoded_response)
+            self.ping_pong()
         else:
-            pass
+            print('[+] Purrbot does not need to respond to a ping')
 
     def post_in_channel(self, chat_string=''):
         if len(chat_string) == 0:
