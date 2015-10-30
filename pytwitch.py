d7dd20767e94186afdad5e90d2aa8edffa062270
Simon AM <simon@agius-muscat.net>
2015-10-30 22:20:59 +0100

    Added donation scrape

--- pytwitch.py
+++ pytwitch.py
@@ -1,5 +1,9 @@
 import socket
+import sqlite3
+from datetime import datetime
 from winsound import Beep
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from time import sleep
 from cfg import *
 
@@ -27,9 +31,17 @@ INITIAL_BUFFER_SIZE = 4098
 GITHUB_URL = r'https://github.com/Winter259/twitch-charity-bot/tree/charity-stream'
 CHECK_TICK = 5
 CYCLES_FOR_PROMPT = 15
+# Stream specific
+CHARITY_URL = r'http://pmhf3.akaraisin.com/specialevents/RibbonofHope'  # PLACEHOLDER FOR TESTING
+STREAMERS = ['purrcat259']
 
 def get_donation_amount():
-    pass
+    print('[+] Purrbot is scraping the url...')
+    data = urlopen(CHARITY_URL).read()
+    soup = BeautifulSoup(data, 'lxml')
+    td = soup.findAll('td', {'class': 'ThermometerAchived', 'align': 'Right'})  # lol badly spelt class
+    achieved_amount = td[0].text  # get just the text
+    return achieved_amount
 
 def get_time_left():
     pass
@@ -44,16 +56,24 @@ class Twitch:
         self.channel = channel
         self.connection = socket.socket()
         self.cycle_count = 0
-        print('[!] Purrbot initialised and connected!')
+        """
+        self.connect(channel)
         print('[+] Initial buffer content:')
         self.print_response(INITIAL_BUFFER_SIZE)
+        """
 
     def run(self):
+        current_money_raised = get_donation_amount()
         while True:
             print('[+] Purrbot is on cycle: {}'.format(self.cycle_count))
             # get donation amount
-            get_donation_amount()
+            new_money_raised = get_donation_amount()
             # if donation amount has changed, post the prompt
+            if not new_money_raised == current_money_raised:
+                print('[!] Purrbot has detected a new donation!')
+                # create a string to post in channels
+                chat_string = 'NEW DONATION! {} has been raised! Visit: {} to donate!'.format(new_money_raised, CHARITY_URL)
+
             # if not, check if cycle count has exceeded the amount required for a prompt
             # if not, check for purrbot info command
             # if not, wait for the check tick
@@ -84,14 +104,21 @@ class Twitch:
         decoded_response = self.receive_data(buffer)
         print('Response: {}'.format(decoded_response))
 
-    def post_in_channel(self, chat_string=''):
+    def post_in_channel(self, channel='',chat_string=''):
+        if len(channel) == 0:
+            print('[-] No channel passed to post string!')
+            return False
+        self.connect(channel)
+        print('[+] Initial buffer content:')
+        self.print_response(INITIAL_BUFFER_SIZE)
         if len(chat_string) == 0:
             print('[-] No string passed to be posted to the chat!')
+            return False
         else:
             print('[?] Attempting to post the string:')
             print('\t', chat_string)
             try:
-                self.connection.send('PRIVMSG {} :{}\r\n'.format(self.channel, chat_string).encode('utf-8'))
+                self.connection.send('PRIVMSG {} :{}\r\n'.format(channel, chat_string).encode('utf-8'))
                 print('[!] String posted successfully!')
                 return True
             except Exception as e:
