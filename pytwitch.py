9fa47a0517ebbe249e341ad32dad144569bbf980
Simon AM <simon@agius-muscat.net>
2015-10-30 21:51:32 +0100

    Finished twitch class refactor

--- pytwitch.py
+++ pytwitch.py
@@ -25,29 +25,29 @@ PORT = 6667             # always use port 6667!
 DATA_BUFFER_SIZE = 1024
 INITIAL_BUFFER_SIZE = 4098
 GITHUB = r'https://github.com/Winter259/twitch-charity-bot/tree/charity-stream'
-PROMPT_DELAY = 60 * 15
+CHECK_TICK = 5
+CYCLES_FOR_PROMPT = 15
 
 class Twitch:
     def __init__(self, name='', token='', channel=''):
         self.name = name
         self.token = token
+        self.channel = channel
         self.connection = socket.socket()
-        self.cycle = 0
+        self.cycle_count = 0
         print('[!] Purrbot initialised and connected!')
         print('[+] Initial buffer content:')
         self.print_response(INITIAL_BUFFER_SIZE)
 
     def run(self):
         while True:
-            print('[+] Purrbot is on cycle: {}'.format(self.cycle))
-            decoded_response = self.receive_data()  # get response once
-            # TODO if the response matches something like a command or a link, respond instantly
-            print_list('Current queue:', self.queue)
-            current_prompt = self.queue.pop(0)
-            print('[+] Purrbot has popped {} from the front of the queue'.format(current_prompt))
-            self.post_in_channel(current_prompt)
-            pause('[+] Holding for next cycle', PROMPT_DELAY)
-            self.cycle += 1
+            print('[+] Purrbot is on cycle: {}'.format(self.cycle_count))
+            # get donation amount
+            # if donation amount has changed, post the prompt
+            # if not, check if cycle count has exceeded the amount required for a prompt
+            # if not, wait for the check tick
+            pause('[+] Holding for next cycle', CHECK_TICK)
+            self.cycle_count += 1
 
     def connect(self, channel=''):
         if len(channel) == 0:
@@ -61,7 +61,7 @@ class Twitch:
                 print('[+] Purrbot has successfully connected to the twitch irc channel: {}'.format(channel))
                 return channel
             except Exception as e:
-                print('[-] Bot did not manage to connect! Exception occurred: {}'.format(e))
+                print('[-] Purrbot did not manage to connect! Exception occurred: {}'.format(e))
                 exit(0)
 
     def receive_data(self, buffer=DATA_BUFFER_SIZE):
@@ -73,15 +73,12 @@ class Twitch:
         decoded_response = self.receive_data(buffer)
         print('Response: {}'.format(decoded_response))
 
-    def ping_pong(self):
-        print('[!] Purrbot has confirmed its connection to the twitch IRC. Checks so far: {}'.format(self.connection_checks))
-        self.connection.send('PONG :tmi.twitch.tv\r\n'.encode('utf-8'))
-
     def post_in_channel(self, chat_string=''):
         if len(chat_string) == 0:
             print('[-] No string passed to be posted to the chat!')
         else:
-            print('[?] Attempting to post the string: {}'.format(chat_string))
+            print('[?] Attempting to post the string:')
+            print('\t', chat_string)
             try:
                 self.connection.send('PRIVMSG {} :{}\r\n'.format(self.channel, chat_string).encode('utf-8'))
                 print('[!] String posted successfully!')
