faf75c2d1b5ad326c0fcd7ac66f54a157ecac7dd
Simon AM <simon@agius-muscat.net>
2015-11-14 00:08:15 +0100

    Optimised plot script

--- plot.py
+++ plot.py
@@ -1,31 +1,26 @@
 import sqlite3
+import matplotlib.pyplot as plt
 
 dbcon = sqlite3.connect('ggforcharity.db')
 dbcur = dbcon.cursor()
-print('Current data:')
 data = dbcur.execute('SELECT * FROM donations')
-
-import matplotlib.pyplot as plt
 
 data_list = []
 for row in data:
     data_list.append(row)
-    print(row)
+    #print(row)
 
 time_list = []
-print('Times:')
 for data in data_list:
     time_list.append(data[3])
-    print(data[3])
+    #print(data[3])
 
 money_list = []
-print('Money;')
 for data in data_list:
     money_list.append(data[2])
-    print(data[2])
+    #print(data[2])
 
 float_list = []
-print('Floats:')
 for money in money_list:
     float_str = ''
     for letter in money:
@@ -36,7 +31,7 @@ for money in money_list:
     if float_str == '110':
         float_str += '.00'
     float_list.append(float(float_str))
-    print(float_str)
+    #print(float_str)
 
 plt.plot(time_list, float_list)
 plt.show()
