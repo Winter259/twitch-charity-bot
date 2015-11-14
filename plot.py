97f67be9f7b2cfcf5c9cd95855186e029f6f2a1d
Simon AM <simon@agius-muscat.net>
2015-11-14 09:57:12 +0100

    Added proper axes to plot script

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 import sqlite3
+import time
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 dbcon = sqlite3.connect('ggforcharity.db')
 dbcur = dbcon.cursor()
@@ -14,7 +16,14 @@ time_list = []
 for data in data_list:
     time_list.append(data[3])
     #print(data[3])
-
+"""
+time_strings = []
+for time_epoch in (time_list[0], time_list[len(time_list) - 1]):
+    time_struct = time.gmtime(time_epoch)
+    time_stamp = time.strftime('%d/%m/%Y %H:%M:%S', time_struct)
+    print(time_stamp)
+    time_strings.append(time_stamp)
+"""
 money_list = []
 for data in data_list:
     money_list.append(data[2])
@@ -33,5 +42,13 @@ for money in money_list:
     float_list.append(float(float_str))
     #print(float_str)
 
+plt.title('GGforCharity Donations over Time')
+#plt.xlabel('time: {} to {}'.format(time_strings[0], time_strings[1]))
+plt.xlabel('time: 12/11/15 22:23 to 16/11/15 00:00')
+plt.ylabel('donations ($CAD)')
+current_time_epoch = int(time.mktime(datetime.now().timetuple()))
 plt.plot(time_list, float_list)
+plt.xlim(1447367000, 1447632000)
+plt.ylim(0, 5000)
+plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.show()
