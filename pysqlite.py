50b9beaf7ca625a1eba3c4deec245c5ff4a1426c
Simon AM <simon@agius-muscat.net>
2016-01-28 22:19:23 +0100

    Updated pysqlite

--- pysqlite.py
+++ pysqlite.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
 
+version = '0.1'
+
 
 class PysqliteError(Exception):
     def __init__(self, value):
@@ -19,6 +21,12 @@ class Pysqlite:
         else:
             raise PysqliteError('{} could not be found or cannot be accessed!'.format(self.dbname))
 
+    def execute_sql(self, execution_string):
+        try:
+            self.dbcur.execute(execution_string)
+        except Exception as e:
+            raise PysqliteError('Pysqlite exception: {}'.format(e))
+
     def get_db_data(self, table):
         try:
             db_data = self.dbcur.execute('SELECT * FROM {}'.format(table))
@@ -31,12 +39,44 @@ class Pysqlite:
             raise PysqliteError('Pysqlite found no data in the table: {} in the DB: {}'.format(table, self.dbname))
         return data_list
 
+    def get_specific_db_data(self, table, filter_string=''):
+        try:
+            db_data = self.dbcur.execute('SELECT * FROM {} WHERE {}'.format(table, filter_string))
+        except Exception as e:
+            raise PysqliteError('Pysqlite experienced the following exception: {}'.format(e))
+        data_list = []
+        for row in db_data:
+            data_list.append(row)
+        if len(data_list) == 0:
+            raise PysqliteError('Pysqlite found no data in the table: {} in the DB: {} using the filter: {}'.format(
+                table,
+                self.dbname,
+                filter_string
+            ))
+        return data_list
+
     def insert_db_data(self, table, row_string, db_data):
         try:
             self.dbcur.execute('INSERT INTO {} VALUES {}'.format(table, row_string), db_data)
             self.dbcon.commit()
         except Exception as e:
             raise PysqliteError('Pysqlite experienced the following exception: {}'.format(e))
+
+    def insert_rows_to_db(self, table, row_string, db_data_list):
+        if len(db_data_list) == 0:
+            raise PysqliteError('Pysqlite received no data to input')
+        if len(db_data_list) == 1:
+            self.insert_db_data(table, row_string, db_data_list[0])
+        else:
+            for data_row in db_data_list:
+                try:
+                    self.dbcur.execute('INSERT INTO {} VALUES {}'.format(table, row_string), data_row)
+                except Exception as e:
+                    raise PysqliteError('Pysqlite could not insert a row: {}'.format(e))
+                try:
+                    self.dbcon.commit()
+                except Exception as e:
+                    raise PysqliteError('Pysqlite could not commit the data: {}'.format(e))
 
 if __name__ == '__main__':
     ggforcharity_db = Pysqlite('GGforCharity', 'ggforcharity.db')
